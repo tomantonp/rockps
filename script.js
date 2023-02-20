738abbe7560df1884448ca5471c43dddf2519e12
tomantonp <tomapopiolek@gmail.com>
2023-02-20 20:31:49 +0300

    Play around with input field and playerChoice but not finalise

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ const strings = ["rock", "paper", "scissors"];
 const computerChoice = strings[randomInt];
 
 console.log (computerChoice);
+
+const playerChoice = document.getElementById("playerChoice").value;
+
+console.log (playerChoice)
+
+//document.getElementById("button").onclick=function () {
+  //  if (document.getElementById("answer")=="rock", computerChoice=="rock"
