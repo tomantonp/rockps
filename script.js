8cd16d3e9f05b03827ac6bdbf1fbde08de900d59
tomantonp <tomapopiolek@gmail.com>
2023-02-21 14:25:36 +0300

    Try adding loop but doesn't keep count and display properly

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 
-const randomInt = Math.floor (Math.random()*3);
+// function getComputerChoice () {
+  
+
+let computerWins = 0;
+  let playerWins = 0;
+
+  console.log (playerWins);
+console.log (computerWins);
+
+for (let i = 0; i < 5; i++) {
+  // your code here!
+
+  const randomInt = Math.floor (Math.random()*3);
 
 const strings = ["rock", "paper", "scissors"];
 
@@ -7,21 +19,39 @@ const computerChoice = strings[randomInt];
 
 console.log (computerChoice);
 
-let one = window.prompt("choose your weapon");
+// }
 
-console.log (one);
+let playerChoice = window.prompt("choose your weapon");
 
-if (one == "rock" && computerChoice == "scissors" || one == "paper" && computerChoice == "rock" || one == "scissors" && computerChoice == "paper") {
+console.log (playerChoice);
+
+
+
+
+// function playRound () {
+
+if (playerChoice == "rock" && computerChoice == "scissors" || playerChoice == "paper" && computerChoice == "rock" || playerChoice == "scissors" && computerChoice == "paper") {
   document.write ("You win! The computer chose " +computerChoice);
+  playerWins ++;
 }
 
-if (one == computerChoice ) {
+if (playerChoice == computerChoice ) {
   document.write ("You draw. The computer chose " +computerChoice ," as well.");
 }
 
-if (one == "rock" && computerChoice == "paper" || one == "paper" && computerChoice == "scissors" || one == "scissors" && computerChoice == "rock") {
+if (playerChoice == "rock" && computerChoice == "paper" || playerChoice == "paper" && computerChoice == "scissors" || playerChoice == "scissors" && computerChoice == "rock") {
   document.write ("You lose. The computer chose " +computerChoice);
+  computerWins ++;
 }
+
+// }
+
+
+
+}
+
+
+
 
 //document.getElementById("button").onclick=function () {
   //  if (document.getElementById("answer")=="rock", computerChoice=="rock"
