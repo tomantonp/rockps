4f23f267e49d3174416306bdf41f48c83346562e
tomantonp <tomapopiolek@gmail.com>
2023-02-21 13:31:41 +0300

    Use prompt function and if statements to play one round

--- script.js
+++ script.js
@@ -7,9 +7,21 @@ const computerChoice = strings[randomInt];
 
 console.log (computerChoice);
 
-const playerChoice = document.getElementById("playerChoice").value;
+let one = window.prompt("choose your weapon");
 
-console.log (playerChoice)
+console.log (one);
+
+if (one == "rock" && computerChoice == "scissors" || one == "paper" && computerChoice == "rock" || one == "scissors" && computerChoice == "paper") {
+  document.write ("You win! The computer chose " +computerChoice);
+}
+
+if (one == computerChoice ) {
+  document.write ("You draw. The computer chose " +computerChoice ," as well.");
+}
+
+if (one == "rock" && computerChoice == "paper" || one == "paper" && computerChoice == "scissors" || one == "scissors" && computerChoice == "rock") {
+  document.write ("You lose. The computer chose " +computerChoice);
+}
 
 //document.getElementById("button").onclick=function () {
   //  if (document.getElementById("answer")=="rock", computerChoice=="rock"
